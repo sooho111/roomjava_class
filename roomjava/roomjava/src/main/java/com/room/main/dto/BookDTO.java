51e74326b62b8a420031bdd8db2cc24918deba48
DESKTOP-KCG86FA\82109 <ehdrbs0217@naver.com>
2021-12-20 09:00:22 +0900

    .

---
+++ roomjava/roomjava/src/main/java/com/room/main/dto/BookDTO.java
@@ -0,0 +1,17 @@
+package com.room.main.dto;
+
+import lombok.Data;
+
+@Data
+public class BookDTO {
+	
+	private String 	book_order;		// 예약번호
+	private String 	book_name;		// 예약자 이름
+	private int	 	book_people;	// 예약 인원
+	private String 	book_tel;		// 예약자 전화번호
+	private String 	book_ok;		// 예약 상태(입금여부-가예약,예약완료,입실,퇴실)
+	private String 	start_date;		// 예약시작일자
+	private String 	end_date;		// 예약종료일자
+	private int		r_bno;			// 방 번호
+	
+}
